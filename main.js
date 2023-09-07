b83cece9cc26a607321ac8f36b59a823f9a56b01
PhillipLDaae1 <e-phidaa@iskule.no>
2023-09-07 14:11:42 +0200

    Should be done

--- main.js
+++ main.js
@@ -35,6 +35,35 @@ getUser = () => {
         user.passord = password;
 
         console.log(user);
+
+        let exportElement = document.getElementById("user_export")
+
+        i = 0;
+
+        while (i < Object.keys(user).length) {
+            exportElement.appendChild(document.createElement("p"));
+            exportElement.lastChild.innerHTML = Object.keys(user)[i] + ":" + Object.values(user)[i];
+            i++;
+        }
+
+        exportElement.classList.add("export");
+
+        let input1 = document.getElementById("fornavn");
+        let input2 = document.getElementById("etternavn");
+        let input3 = document.getElementById("adresse");
+        let input4 = document.getElementById("postnummer");
+        let input5 = document.getElementById("poststed");
+        let input6 = document.getElementById("telefon");
+        let input7 = document.getElementById("passord");
+
+        input1.value = "";
+        input2.value = "";
+        input3.value = "";
+        input4.value = "";
+        input5.value = "";
+        input6.value = "";
+        input7.value = "";
     };
     
 };
+
